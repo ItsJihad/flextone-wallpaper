a0515e468f1870d9394d3d714c87f0bb3f78cafb
itsjihad <itsjihad@proton.me>
2024-10-20 02:45:19 +0600

    editing maccy menu today

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+    zIndex: {
+      '100': '100',
+    }
+  },
+  plugins: [],
+}
+
